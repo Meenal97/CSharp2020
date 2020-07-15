f83b1996e93dc89ea5135dd3ec9f797dc9eb4b20
MeenalKaurLNG <meenalpreet.kaur@lngconsultancy.co.in>
2020-07-15 22:25:54 +0530

    Interface

---
+++ Oops/InterfaceAsObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasic.Oops
+{
+
+        interface IA
+        {
+            void print();
+        }
+        interface IB
+        {
+            void print();
+        }
+        class classAB : IA, IB
+        {
+            void IA.print()
+            {
+                Console.WriteLine("Interface A, Class AB");
+            }
+
+            void IB.print()
+            {
+                Console.WriteLine("Interface B, Class AB");
+            }
+        }
+    
+}
