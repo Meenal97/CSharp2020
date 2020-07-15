fc42b21c7f9cdcd25555e32d25fbb9fb21378231
Meenal97 <meenalpreet94@gmail.com>
2020-07-15 22:52:23 +0530

    Abstract Class

---
+++ Oops/AbstractClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasic.Oops
+{
+   public abstract class abstractClass
+    {
+        public virtual void details()
+        {
+            Console.WriteLine("Details Method, Defined in abstract class");
+        }
+        public abstract void print();
+    }
+
+    public class studentclass : abstractClass
+    {
+        public override void print()
+        {
+            Console.WriteLine("Print Class");
+        }
+
+        public override void details()
+        {
+            Console.WriteLine("Details Method, Overriden ");
+        }
+
+    }
+}
