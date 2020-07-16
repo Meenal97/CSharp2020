d9c9eecdd13e590c3c7eafcbf8fc1662421b3d97
Meenal97 <meenalpreet94@gmail.com>
2020-07-16 23:02:07 +0530

    Why Enum

---
+++ WhyEnums.cs
@@ -0,0 +1,16 @@
+﻿using CSharpBasic.Oops;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBasic
+{
+    public class WhyEnums
+    {
+        public string Name { get; set; }
+        public int Gender { get; set; }
+
+    }
+
+    
+}
