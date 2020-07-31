0c8bf459f9c5556d01949cb7690462b3e9cf8a6d
MeenalKaurLNG <meenalpreet.kaur@lngconsultancy.co.in>
2020-07-31 15:31:26 +0530

    Indexers

--- AccessModifiers1.cs
+++ AccessModifiers1.cs
@@ -4,32 +4,32 @@ using System.Text;
 
 namespace CSharpBasic
 {
-    public class AccessModifiers1
-    {
-        private int _ID;
-        protected string _lastname;
-        public int ID { get; set; }
-        internal string first_Name;
-    }
+    //public class AccessModifiers1
+    //{
+    //    private int _ID;
+    //    protected string _lastname;
+    //    public int ID { get; set; }
+    //    internal string first_Name;
+    //}
 
-    public class AccessModifiersclass2 : AccessModifiers1
-    {
-       public void PrintID()
-        {
-            AccessModifiersclass2 AID = new AccessModifiersclass2();
-           // AID._id = 101;
-            AID._lastname = "Meenal";
-            Console.WriteLine("Name {0}", AID._lastname);
-            base._lastname = "Juhi";
-            Console.WriteLine("Name {0}", base._lastname);
-            this._lastname = "Neha";
-            Console.WriteLine("Name {0}", this._lastname);
-        }
+    //public class AccessModifiersclass2 : AccessModifiers1
+    //{
+    //   public void PrintID()
+    //    {
+    //        AccessModifiersclass2 AID = new AccessModifiersclass2();
+    //       // AID._id = 101;
+    //        AID._lastname = "Meenal";
+    //        Console.WriteLine("Name {0}", AID._lastname);
+    //        base._lastname = "Juhi";
+    //        Console.WriteLine("Name {0}", base._lastname);
+    //        this._lastname = "Neha";
+    //        Console.WriteLine("Name {0}", this._lastname);
+    //    }
        
-        public void  p()
-        {
-            base._lastname = "Water";
-            Console.WriteLine("P {0}", base._lastname);
-        }
-    }
+    //    public void  p()
+    //    {
+    //        base._lastname = "Water";
+    //        Console.WriteLine("P {0}", base._lastname);
+    //    }
+    //}
 }
---
+++ Indexers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharpBasic
+{
+    public  class Employee23
+    {
+        public int EmployeeID { get; set; }
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+    }
+
+    public class Company23
+    {
+        List<Employee23> listEmp;
+       
+        public Company23()
+        {
+            listEmp = new List<Employee23>();
+            listEmp.Add(new Employee23 { EmployeeID = 1, Name = "Mike", Gender = "Male" });
+            listEmp.Add(new Employee23 { EmployeeID = 2, Name = "John", Gender = "Male" });
+            listEmp.Add(new Employee23 { EmployeeID = 3, Name = "Raine", Gender = "Female" });
+            listEmp.Add(new Employee23 { EmployeeID = 4, Name = "Ben", Gender = "Female" });
+            listEmp.Add(new Employee23 { EmployeeID = 5, Name = "Todd", Gender = "Male" });
+            listEmp.Add(new Employee23 { EmployeeID = 6, Name = "Maxine", Gender = "Female" });
+            listEmp.Add(new Employee23 { EmployeeID = 7, Name = "Emiliy", Gender = "Female" });
+            listEmp.Add(new Employee23 { EmployeeID = 8, Name = "Mell", Gender = "Female" });
+        }
+      
+        public string this[int empID]
+        {
+            get
+            {
+                return listEmp.FirstOrDefault(emp => emp.EmployeeID == empID).Name;
+            }
+            set
+            {
+                listEmp.FirstOrDefault(emp => emp.EmployeeID == empID).Name = value;
+            }
+        }
+    }
+}
